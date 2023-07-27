f0d187567a7ba75a3b961132945a0a4bf6e1e565
Herve Chendjou <hchend2@uic.edu>
2023-07-27 14:09:26 -0500

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+
+//
+//    The heap must satisfy the heap property.
+//    Every parent node in the heap must have 2^x children.
+//    The value of x must be a parameter of the heap’s constructor.
+//    The heap must implement an insert method.
+//    The heap must implement a pop max method.
+//    The heap must be implemented in Java.
+//    The heap must be performant.
+//    You must use a more descriptive variable name than x in your implementation.
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/org/example/PowerOf2MaxHeap.java
@@ -0,0 +1,51 @@
+package org.example;
+import java.util.Arrays;
+
+public class PowerOf2MaxHeap {
+
+    private int size = 0;
+    private int[] MaxHeap; // array
+    private int capacity;
+
+    // constructor ...
+    public PowerOf2MaxHeap(int size) {
+        this.capacity = size;
+        this.size = 0;
+        this.MaxHeap = new int[capacity];
+    }
+
+    //method to get the parent node position ...
+    private int getParentPosition(int i) {
+        return (i/2);
+    }
+
+    // return right child node position ...
+    private int getRightChildPosition(int i) {
+        return(2*i + 1);
+    }
+
+    // return left child node position..
+    private  int getLeftChildPosition(int i) {
+        return (2*i);
+    }
+
+    // re-arrange the heap going upward from children to parent ...
+    // to make sure that the heap properties are respected ...
+    public void reArrangeMaxHeapUpward(int index) {
+        int tmp = MaxHeap[index];
+        while (index > 0 && tmp > MaxHeap[getParentPosition(index)]) {
+            MaxHeap[index] = MaxHeap[getParentPosition(index)];
+            index = getLeftChildPosition(index);
+        }
+        MaxHeap[index] = index;
+    }
+
+    // insert a new node item into the heap and re-arrange the heap ...
+    public void insertNodeItem(int item) {
+        size = size + 1;
+        MaxHeap[size] = item;
+        int currIndex = size;
+        reArrangeMaxHeapUpward(currIndex); // re-arrange the heap as a new item is inserted ...
+    }
+
+}
