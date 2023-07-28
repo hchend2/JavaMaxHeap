41a7c07f7dff1f1d0a500185e9016d834647bb24
Herve Chendjou <hchend2@uic.edu>
2023-07-28 15:11:12 -0500

    isfull, isempty, swap functions added

--- src/main/java/org/example/PowerOf2MaxHeap.java
+++ src/main/java/org/example/PowerOf2MaxHeap.java
@@ -1,51 +1,81 @@
 package org.example;
 import java.util.Arrays;
+import java.lang.Math;
+
+import static java.lang.Math.pow;
 
 public class PowerOf2MaxHeap {
-
-    private int size;
+    private int size = 0;
+//    private  int d;
+    private int numberOfChildren;
     private int[] MaxHeap; // array
     private int capacity;
 
     // constructor ...
-    public PowerOf2MaxHeap(int capacity) {
+    public PowerOf2MaxHeap(int capacity, int n) {
+        this.numberOfChildren = (int) pow(2, n);
         this.capacity = capacity;
-        this.size = 0;
-        this.MaxHeap = new int[capacity];
+        MaxHeap = new int[this.capacity];
+    }
+
+    // get size of the heap array ...
+    public int getSizeOfMaxHeap() {
+        return size;
+    }
+
+    // return true if heap is empty ...
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // return true if heap is full ...
+    public boolean isFull() {
+        return size == MaxHeap.length;
     }
 
     // return the parent node position ...
     private int getParentPosition(int i) {
-        return (i/2);
+        return (i - 1)/2;
     }
 
     // return right child node position ...
     private int getRightChildPosition(int i) {
-        return(2*i + 1);
+        return (2*i + 2);
     }
 
-    // return left child node position..
+    // return left child node position ...
     private  int getLeftChildPosition(int i) {
-        return (2*i);
+        return (2*i + 1);
     }
 
     // re-arrange the heap going upward from children to parent ...
     // to make sure that the heap properties are respected ...
     public void reArrangeMaxHeapUpward(int index) {
         int tmp = MaxHeap[index];
-        while (index > 0 && tmp > MaxHeap[getParentPosition(index)]) {
+        while (index > 0 && tmp < MaxHeap[getParentPosition(index)]) {
             MaxHeap[index] = MaxHeap[getParentPosition(index)];
             index = getParentPosition(index);
         }
-        MaxHeap[index] = index;
+        MaxHeap[index] = tmp;
+    }
+
+    // swap 2 element ...
+    private void swapPosition(int i, int j) {
+        int tmp;
+        tmp = MaxHeap[i];
+        MaxHeap[i] = MaxHeap[j];
+        MaxHeap[j] = tmp;
     }
 
     // insert a new node item into the heap and re-arrange the heap ...
     public void insertNodeItem(int item) {
-        size = size + 1;
         MaxHeap[size] = item;
         int currIndex = size;
-        reArrangeMaxHeapUpward(currIndex); // re-arrange the heap as a new item is inserted ...
+        while (MaxHeap[currIndex] > MaxHeap[getParentPosition(currIndex)]) {
+            swapPosition(currIndex, getParentPosition(currIndex)); // swap elements ...
+            currIndex = getParentPosition(currIndex); // update current position ...
+        }
+        size++;
     }
 
 }
--- src/test/java/MaxHeapTests.java
+++ src/test/java/MaxHeapTests.java
@@ -1,14 +1,40 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.example.PowerOf2MaxHeap;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
+
 public class MaxHeapTests {
 
-    @Test
-    void test() {
-        fail("Not yet implemented");
+//    @Test
+//    void test() {
+//        fail("Not yet implemented");
+//    }
+
+    static PowerOf2MaxHeap max_heap;
+    @BeforeEach
+    void setup() {
+        max_heap = new PowerOf2MaxHeap(10, 2);
     }
+
+    @Test
+    void testSizeofMaxHeap() {
+//      the initial size of the heap when it created.
+        assertEquals(0, max_heap.getSizeOfMaxHeap());
+    }
+    @Test
+    void testInsertNodeItem() {
+//      insert few items into the heap and check if the size increases ...
+        max_heap.insertNodeItem(10);
+        max_heap.insertNodeItem(5);
+        assertEquals(2, max_heap.getSizeOfMaxHeap());
+    }
+
+
 }
